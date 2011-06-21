725d481bc250a0c1e86ac5173a1619533708076c
Gregg Gajic <gregggajic@gmail.com>
2011-06-20 21:37:28 -0500

    Remove tests/build_tests.sh

--- tests/build_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-g++ main.cc benchmark.cc raywevaluator.cc -o tests -O3 -I /data/tmp/boost_1_46_0/include -L /data/tmp/boost_1_46_0/boost_1_46_0/lib -lboost_serialization-gcc4-mt-1_46_0 -lboost_filesystem-gcc4-mt-1_46_0 -lboost_system-gcc4-mt-1_46_0 -lboost_unit_test_framework-gcc4-mt-1_46_0
